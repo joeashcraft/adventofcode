379b19c267dc4be1e0df56e4f60bf33e73e156e5
Joe Ashcraft <joe.ashcraft@rackspace.com>
2018-12-04 11:02:16 -0600

    remove prompt from test file

--- day2-test.py
+++ day2-test.py
@@ -1,23 +1,6 @@
 import unittest
 from day2 import checksum, letterCount, part2
 
-"""For example, if you see the following box IDs:
-
-* abcdef contains no letters that appear exactly two or three times.
-* bababc contains two a and three b, so it counts for both.
-* abbcde contains two b, but no letter appears exactly three times.
-* abcccd contains three c, but no letter appears exactly two times.
-* aabcdd contains two a and two d, but it only counts once.
-* abcdee contains two e.
-* ababab contains three a and three b, but it only counts once.
-
-Of these box IDs, four of them contain a letter which appears exactly twice,
-and three of them contain a letter which appears exactly three times.
-
-Multiplying these together produces a checksum of 4 * 3 = 12.
-"""
-
-
 class MyFirstTests(unittest.TestCase):
     def test_myHelpers(self):
         self.assertEqual(letterCount('bababc'), {'a': 2, 'b': 3, 'c': 1})
