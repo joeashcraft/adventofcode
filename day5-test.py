4a601c90c4fe6e13a9b9c34078903e141ec72b92
Joe Ashcraft <joe.ashcraft@rackspace.com>
2018-12-29 21:53:30 -0600

    a much better day5 part1 solution stolen from reddit u/eltrufas

--- day5-test.py
+++ day5-test.py
@@ -1,16 +1,25 @@
 import unittest
-from day5 import part1
+from day5 import react, part1, part1_test_if_pair
 from day5 import part2
 
 
 class MyFirstTests(unittest.TestCase):
     test_input = 'dabAcCaCBAcCcaDA'
 
-    def test_part1(self):
-        self.assertEqual(len(part1(self.test_input)), 10)
+    def test_part1_helpers(self):
+        self.assertEqual(part1_test_if_pair('A', 'A'), False)
+        self.assertEqual(part1_test_if_pair('A', 'a'), True)
+        self.assertEqual(part1_test_if_pair('a', 'A'), True)
+        self.assertEqual(part1_test_if_pair('a', 'z'), False)
 
-    def test_part2(self):
-        self.assertEqual(part2(self.test_input), 4)
+    def test_part1(self):
+        self.assertEqual(len(react("aaaaa")), 5)
+        self.assertEqual(len(react("aBcDe")), 5)
+        self.assertEqual(len(react("aAbcd")), 3)
+        self.assertEqual(len(react(self.test_input)), 10)
+
+    # def test_part2(self):
+    #     self.assertEqual(part2(self.test_input), 4)
 
 
 if __name__ == '__main__':
--- day5.py
+++ day5.py
@@ -1,6 +1,28 @@
 #!env python
 
 
+def part1_test_if_pair(x, y):
+    if (x.lower() == y.lower()) and \
+        (
+            (x.isupper() and y.islower()) or
+            (x.islower() and y.isupper())
+    ):
+        return True
+    else:
+        return False
+
+def react(line):
+    buf = []
+    # print("BEGIN")
+    for c in line:
+        # print("  for loop: {} of {}".format(c, len(line)-1))
+        # print(buf)
+        if buf and part1_test_if_pair(c, buf[-1]):
+            buf.pop()
+        else:
+            buf.append(c)
+    return buf
+
 def part1(s):
     """What remains after fully reacting the polymer?
     The polymer is formed by smaller units which, when triggered, react with each other such that two adjacent units of the same type and opposite polarity are destroyed.
@@ -13,25 +35,32 @@ def part1(s):
 #         print("done")
 #         return s
     # print("Reacting {}".format(''.join(wip_input)))
-    for c, v in enumerate(wip_input):
-        if c > len(wip_input) - 2:
-            print("done 1")
-            break
+    for c in range(len(wip_input)):
+        print("for loop: {} of {} for {}".format(c+1, len(wip_input)-1, wip_input))
         test_pair = wip_input[c:c + 2]
-        # print("Considering Pair {}: {}".format(c, ''.join(test_pair)))
-        if test_pair[0].lower() == test_pair[1].lower():
-            # print("Pair {} {} is a candidate".format(c, ''.join(test_pair)))
-            if (test_pair[0].isupper() and test_pair[1].islower()) or (test_pair[0].islower() and test_pair[1].isupper()):
-                # print("Pair {} {} have reacted".format(c, ''.join(test_pair)))
-                wip_input.pop(c)
-                wip_input.pop(c)
-                # print("After a Reaction: {}".format(''.join(wip_input)))
-                return part1(''.join(wip_input))
-            # else:
-                # print("Pair {} {} have NOT reacted".format(c, ''.join(test_pair)))
-        elif c + 2 == len(wip_input):
-            # print("done 2")
-            return s
+        if len(test_pair) < 2:
+            print("    done 1")
+            return wip_input
+        print("  Considering Pair {}: {}".format(c+1, ''.join(test_pair)))
+        if part1_test_if_pair(*test_pair):
+            print("    Pair {} {} have reacted".format(c+1, ''.join(test_pair)))
+            wip_input.pop(c)
+            wip_input.pop(c)
+            print("    After a Reaction: {}".format(''.join(wip_input)))
+            # return part1(''.join(wip_input))
+            continue
+        else:
+            print("    Pair {} {} have NOT reacted".format(c+1, ''.join(test_pair)))
+            if c+2 >= len(wip_input):
+                print("    done - reached end")
+                return wip_input
+            else:
+                print("    done - catch all")
+            # print('.', end='')
+        # else:
+        #     return part1(''.join(wip_input))
+    # else:
+        # return wip_input
 
 
 def part2(s):
@@ -50,6 +79,9 @@ if __name__ == '__main__':
     file.close()
 
     input = linesin.splitlines()[0]
-
     print("{} units remain after fully reacting the polymer.".format(
-        len(part1(input))))
+        len(react(input))))
+    # print("{} units remain after fully reacting the polymer.".format(
+        # len(part1(input))))
+    # print("{} is the shortest polymer if all of one type is removed.".format(
+    #     part2(input)))
