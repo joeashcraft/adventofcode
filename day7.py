f522e82374102bb49488954680151ae79390c1c4
Joe Ashcraft <joeashcraft@gmail.com>
2019-11-17 00:42:47 -0600

    Day 7 -- a working solution, not pretty

--- day7.py
+++ day7.py
@@ -1,5 +1,13 @@
 #!env python
+from logzero import logger
 
+
+class OrderedSet(list):
+    def append_dedup(self, *args):
+        for x in args:
+            if x not in self:
+                self.append(x)
+        return
 
 class Step(str):
     pass
@@ -28,50 +36,65 @@ def parse_instructions(raw_instructions: list) -> list:
 
 
 def part1(raw_instructions: list) -> str:
-    final_order = []
+    final_order = OrderedSet()
     valid_steps = set()
     parsed_instructions = list()
 
     # reformat raw ins for easier processing
     parsed_instructions = parse_instructions(raw_instructions)
+    logger.debug(f"parsed_instructions ({len(parsed_instructions)}): {parsed_instructions}")
 
     # create a non-dupe list of valid steps
     for instruction in parsed_instructions:
         valid_steps.add(instruction[0])
         valid_steps.add(instruction[1])
     valid_steps: list = sorted(valid_steps)
+    valid_steps = {x:0 for x in valid_steps}
+    logger.debug(f"valid_steps ({len(valid_steps)}): {valid_steps}")
+
 
     # starting at VALID[0], check for any other INS where CURR is second
-    step = valid_steps[0]
+    # might as well start at the beginning
+    step = sorted(valid_steps.keys())[0]
+    logger.debug(f"step: {step}")
+
+    wcount = 0
+    logger.debug(f"WHILE: {wcount}")
     while len(valid_steps):
-        before_steps = []
-        # e.g. for c, find all instructions which must finish before C
-        before_steps = [x for x, y in parsed_instructions if step == y]
-        # if found, then go to first ins in alpha order, repeat
-        if len(before_steps):
-            step = sorted(before_steps)[0]
-            continue
-        else:
-            # if not found, do it, and go to next INS in VALID
-            final_order.append(step)
-            try:
-                valid_steps.remove(step)
-            except ValueError:
-                True
-            for instruction in parsed_instructions:
-                if instruction[0] == step:
-                    parsed_instructions.remove(instruction)
-            try:
-                step = valid_steps[0]
-            except IndexError:
-                True
-            continue
+        wcount += 1
+        logger.debug(f"WHILE: {wcount}")
+
+        # count dependant valid_steps
+        for k, v in valid_steps.items():
+            count = int(0)
+            for x, y in parsed_instructions:
+                if k == y:
+                    count += 1
+            valid_steps[k] = count
+        logger.debug(f"valid_steps ({len(valid_steps)}): {valid_steps}")
+
+        # what's ready to exec?
+        least = min([v for k, v in valid_steps.items()])
+        # sort and exec first
+        ready = sorted([k for k, v in valid_steps.items() if v == least])[0]
+        logger.debug(f"step: {ready}")
+        final_order.append(ready)
+        logger.debug(f"final_order ({len(final_order)}): {final_order}")
+        valid_steps.pop(ready)
+        logger.debug(f"valid_steps ({len(valid_steps)}): {valid_steps}")
+        for ii in parsed_instructions.copy():
+            # breakpoint()
+            if ii[0] == ready:
+                parsed_instructions.remove(ii)
+                logger.debug(f"parsed_instructions ({len(parsed_instructions)}): {parsed_instructions}")
+
 
 
     # at end of parsed, run remaining VALID in alpha order
-    return final_order
+    return ''.join(final_order)
 
 if __name__ == '__main__':
+    logger.info("Executing as script")
     file = open('day7-input.txt', 'r')
     raw_instructions: list = file.readlines()
     file.close()
