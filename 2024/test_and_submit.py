f5f208cf375732b9b33f84a30c51f1b4e27a056a
Joe Ashcraft <joeashcraft@gmail.com>
2024-12-03 23:00:42 -0600

    update - optional Part-# for test-and-submit script

--- 2024/test_and_submit.py
+++ 2024/test_and_submit.py
@@ -15,7 +15,7 @@ YEAR = 2024
 
 def init_argparse():
     parser = argparse.ArgumentParser(
-        usage="%(prog)s [--dry-run] day",
+        usage="%(prog)s [--dry-run] day [part]",
         description="Run the tests; if tests succeed, submit answer to adventofcode.com.",
     )
     parser.add_argument(
@@ -25,6 +25,7 @@ def init_argparse():
         action="store_true",
     )
     parser.add_argument("day", type=int)
+    parser.add_argument("part", type=int, default=0)
 
     return parser
 
@@ -54,12 +55,13 @@ def split_parts_of_solution(solution: list) -> list:
 if __name__ == "__main__":
     parser = init_argparse()
     if DEBUG:
-        args = parser.parse_args(["--dry-run", "1"])
+        args = parser.parse_args(["--dry-run", "1", "0"])
     else:
         args = parser.parse_args()
     logger.debug(args)
 
     ## ONE: run tests
+    logger.info(f"[1] Run Tests")
     from os import system, popen
     from sys import exit
 
@@ -69,10 +71,9 @@ if __name__ == "__main__":
         logger.fatal("tests failed, not proceeding to Step Two")
         if not DEBUG:
             exit(tests_result)
-    # exec(open(f"day{args.day}_test.py").read())
 
     ## TWO: run the real thing
-    logger.info(f"start Step Two")
+    logger.info(f"[2] Run the Real Thing")
     solutions_result = popen(f"python3 day{args.day}.py").read()
 
     part1_solution, part2_solution = split_parts_of_solution(solutions_result)
@@ -80,17 +81,20 @@ if __name__ == "__main__":
     logger.debug(f"part2_solution is {part2_solution}")
 
     ## THREE: check the answer
-    if part1_solution:
+    logger.info(f"[3] Submit Solutions")
+    if part1_solution and args.part in [0, 1]:
         logger.debug("submit part1")
         if args.dry_run:
             logger.debug(
                 f"[DRY RUN] submit_result(year={YEAR}, day={args.day}, part=1, answer={part1_solution})"
             )
-        # submit_result(YEAR, args.day, 1, part1_solution)
-    if part2_solution:
+        else:
+            print(submit_result(YEAR, args.day, 1, part1_solution))
+    if part2_solution and args.part in [0, 2]:
         logger.debug("submit part2")
         if args.dry_run:
             logger.debug(
                 f"[DRY RUN] submit_result(year={YEAR}, day={args.day}, part=2, answer={part2_solution})"
             )
-        # submit_result(YEAR, args.day, 2, part2_solution)
+        else:
+            print(submit_result(YEAR, args.day, 2, part2_solution))
